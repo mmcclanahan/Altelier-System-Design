756cc2f1742a5b8da86a568d398fab649c905a3f
mmcclanahan <mmmcclan365@gmail.com>
2024-03-30 15:40:18 -0700

    removed routes from server

--- Server/server.js
+++ Server/server.js
@@ -4,9 +4,6 @@ const express = require('express');
 const path = require("path");
 const app = express();
 
-//import routes
-const productRoutes = require('./routes/productRoutes');
-
 //middleware
 app.use(express.static(path.join(__dirname, '../public')));
 app.use(express.json());
